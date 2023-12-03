20aba0de7457559a7da197a8b1ef7468b23d3346
walaa alkasim <walaa.alkasim@hotmail.com>
2023-12-03 21:11:21 +0100

    update project

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,15 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 const DB_HOST = process.env.DB_HOST;
 
-const dbURI = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}.h6npx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+// const dbURI =
 
+// `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}.h6npx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+const dbURI =
+// `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}.h6npx.mongodb.net/${DB_NAME}?retryWrites=true&
+`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.h6npx.mongodb.net/w=majority`;
+
+console.log('dbURI',dbURI)
 const connectDB = async () => {
   try {
     await mongoose.connect(dbURI);
